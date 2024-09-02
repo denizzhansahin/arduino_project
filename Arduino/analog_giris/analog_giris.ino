54f9dbac36b9dee66071c136f0a2a1ae12b856bb
Denizhan Şahin <95483485+denizzhansahin@users.noreply.github.com>
2024-09-02 16:37:38 +0300

    Add files via upload

---
+++ Arduino/analog_giris/analog_giris.ino
@@ -0,0 +1,15 @@
+const int analogGiris=A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(analogGiris,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int deger = analogRead(analogGiris);
+  Serial.println("deger:");
+  Serial.println(deger);
+  delay(1000);
+}
